ab3987eb1f0ef6385798a6c8e5aab316b1426dca
salahar9 <salahar9@gmail.com>
2023-01-11 18:48:57 +0100

    new things

--- src/views/IndexSections/TranslateBox.js
+++ src/views/IndexSections/TranslateBox.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Form, TextArea, Button, Icon } from "semantic-ui-react";
 import axios from "axios";
 import apiroute from "APIutils";
+import "semantic-ui-css/semantic.min.css";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function TranslateBox() {
   const [inputText, setInputText] = useState("");
   const [resultText, setResultText] = useState("");
@@ -22,25 +26,36 @@ export default function TranslateBox() {
     setResultText(response.data.translatedText);
   };
   return (
-    <div className="sec2">
-      <Form>
-        <Form.Field
-          control={TextArea}
-          placeholder="Type Text to Translate.."
-          onChange={(e) => setInputText(e.target.value)}
-        />
-
-        <Form.Field
-          control={TextArea}
-          placeholder="Your Result Translation.."
-          value={resultText}
-        />
-      </Form>
-
-      <Button className="translate-button" onClick={translateText}>
-        <Icon name="translate" />
-        Translate
-      </Button>
+    <div className="h-100vh sec2-main">
+      <h1 className="sec2-title"> Use this boxes to translate text </h1>
+      <h1 className="sec2-title"> استعمل الخانة أسفله لترجمة نصوصك</h1>
+      <div className="sec2">
+        <Form>
+          <div className="translate-box">
+            <Form.Field
+              control={TextArea}
+              rows="5"
+              placeholder="Type Text to Translate.."
+              onChange={(e) => setInputText(e.target.value)}
+            />
+            <Button className="translate-button2" onClick={translateText}>
+              {
+                // <i class="fa-regular fa-language"></i>
+                // <FontAwesomeIcon icon={faLifeRing} />
+              }
+              <FontAwesomeIcon icon={faGlobe} />
+            </Button>
+          </div>
+          <Form.Field
+            dir="rtl"
+            control={TextArea}
+            protected="true"
+            rows="5"
+            placeholder="Your Result Translation.."
+            value={resultText}
+          />
+        </Form>
+      </div>
     </div>
   );
 }
